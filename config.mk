a236a1b45c9d0949bfe57d543aa318eaa47ba331
ooohpi <taochangwan@qq.com>
2023-04-18 17:36:00 +0800

    nothing

--- config.mk
+++ config.mk
@@ -3,10 +3,10 @@ VERSION = 5.2
 
 # paths
 PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+MANPREFIX = /usr/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
